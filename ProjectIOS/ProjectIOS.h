f19de94181fc94cd13318a2202d4e92f622e9c2d
Jefwillems <jefwillems100@gmail.com>
2017-12-23 12:43:15 +0100

    Initial Commit

---
+++ ProjectIOS/ProjectIOS.h
@@ -0,0 +1,19 @@
+//
+//  ProjectIOS.h
+//  ProjectIOS
+//
+//  Created by Jef Willems on 23/12/2017.
+//  Copyright © 2017 Jef Willems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProjectIOS.
+FOUNDATION_EXPORT double ProjectIOSVersionNumber;
+
+//! Project version string for ProjectIOS.
+FOUNDATION_EXPORT const unsigned char ProjectIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProjectIOS/PublicHeader.h>
+
+
